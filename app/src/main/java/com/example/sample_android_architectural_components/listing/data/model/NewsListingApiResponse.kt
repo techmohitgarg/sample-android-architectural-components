aad846e38cef05010a46c7124d366c7ca8807866
Mohit Garg <tech.mohit.garg@gmail.com>
2021-04-10 12:40:04 +0530

    Udpate pulgin and remove unwanted changes

--- app/src/main/java/com/example/sample_android_architectural_components/listing/data/model/NewsListingApiResponse.kt
+++ app/src/main/java/com/example/sample_android_architectural_components/listing/data/model/NewsListingApiResponse.kt
@@ -1,14 +1,39 @@
 package com.example.sample_android_architectural_components.listing.data.model
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class NewsListingApiResponse(
-    val status: String, val totalResults: Int, val articles: List<Articles>
-) : Parcelable
+    val status: String, val totalResults: Int, val articles: List<Articles> = emptyList()
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readInt(),
+        parcel.createTypedArrayList(Articles)!!
+    )
 
-@Parcelize
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(status)
+        parcel.writeInt(totalResults)
+        parcel.writeTypedList(articles)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<NewsListingApiResponse> {
+        override fun createFromParcel(parcel: Parcel): NewsListingApiResponse {
+            return NewsListingApiResponse(parcel)
+        }
+
+        override fun newArray(size: Int): Array<NewsListingApiResponse?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+
 data class Articles(
     val author: String,
     val title: String,
@@ -17,4 +42,38 @@ data class Articles(
     val urlToImage: String,
     val publishedAt: String,
     val content: String
-) : Parcelable
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(author)
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeString(url)
+        parcel.writeString(urlToImage)
+        parcel.writeString(publishedAt)
+        parcel.writeString(content)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Articles> {
+        override fun createFromParcel(parcel: Parcel): Articles {
+            return Articles(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Articles?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
